7356f7cb13f98dcac61c33c1b40f0dde8c2891a7
Rasmus Knudsen <rasmuspriv@gmail.com>
2017-01-24 20:11:34 +0100

    Initial commit

---
+++ Assets/WASD.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var movementSpeed: int;
+
+function Start() {
+	var transform: Transform = GetComponent.<Transform>();
+}
+
+function Update() {
+	transform.Translate((Vector3(Input.GetAxis('Horizontal'), 0, Input.GetAxis('Vertical')) * movementSpeed) * Time.deltaTime);
+}
+
+function OnCollisionEnter(collision: Collision) {
+	Debug.Log('Collision detected.');
+}
